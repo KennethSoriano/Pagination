a2424420dc2a80287c20152ad8fcd91ac7d6a97e
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-16 19:41:55 -0700

    fetch/display list rework

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
+  const {loading, data} = useFetch()
 
   return (
   <main>
-    
+    <div className="section-title">
+      <h1>{loading ? 'loading...' : 'pagination'}</h1>
+      <div className="underline"></div>
+    </div>
+    <section className="followers">
+      <div className="container">
+        {data.map((follower) => {
+          <Follower key={follower.id} {...follower} />
+        })}
+      </div>
+    </section>
   </main>
   )
 }
